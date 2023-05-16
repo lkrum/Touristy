48a54c95333f1a8876d7f7d747e3f5171d31aa9f
lkrum <leahkrumholz73@gmail.com>
2023-05-16 11:03:06 -0500

    updated trip model to include post_id. Wrote all the relationships in the index.js file under models

--- models/index.js
+++ models/index.js
@@ -1,12 +1,55 @@
-const User = require("./user");
-const Trip = require("./trip");
+const User = require('./user');
+const Trip = require('./trip');
+const Image = require('./image');
+const Post = require('./post');
 
-User.hasMany(Trip, { 
+// user relationships
+User.hasMany(Trip, Image, Post, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE' 
+  onDelete: 'CASCADE'
 });
 
-Trip.belongsTo(User, { 
-  foreignKey: 'user_id' });
+// trip relationships
+Trip.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
-module.exports = { User, Trip };
+Trip.hasMany(Image, {
+  foreignKey: 'image_id'
+});
+
+Trip.hasMany(Post, {
+  foreignKey: 'post_id'
+});
+
+// image relationships
+Image.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Image.belongsTo(Trip, {
+  foreignKey: 'trip_id'
+});
+
+Image.belongsTo(Post, {
+  foreignKey: 'post_id'
+})
+
+Image.hasMany(Post, {
+  foreignKey: 'post_id'
+})
+
+// post relationships
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(Trip, {
+  foreignKey: 'trip_id'
+})
+
+Post.hasOne(Image, {
+  foreignKey: 'image_id'
+})
+
+module.exports = { User, Trip, Image, Post };
