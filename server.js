b42cdb92f90220701e8c95fd2531334ae03aff00
Walker Logan <walker.logan44@gmail.com>
2023-05-16 13:08:38 -0500

    debugging server.js

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const app = express();
+
+const hbs = exphbs.create();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 const PORT = process.env.PORT || 3001;
 
 const sess = {
